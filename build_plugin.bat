721cbfa1530920255910c1968f00f333085adf78
cfwnico <cfwnico@users.noreply.github.com>
2026-07-18 22:36:24 +0800

    Switch to fb2k_sdk_2025, fix API, update CI

--- build_plugin.bat
+++ build_plugin.bat
@@ -6,11 +6,11 @@ echo ==============================================
 :: Call the Visual Studio Developer Command Prompt environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
 
-:: Build the solution (Win32, Release mode)
-msbuild "e:\LiveLyricOverlay\fb2k_sdk\foobar2000\foo_sample\foo_sample.sln" /p:Configuration=Release /p:Platform=x86 /p:PlatformToolset=v143
+:: Build the project (x64, Release mode)
+msbuild "e:\LiveLyricOverlay\fb2k_sdk_2025\foobar2000\foo_sample\foo_sample.vcxproj" /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v143 /p:TargetName=foo_livelyric
 
 echo.
 echo If the build was successful, your plugin will be located at:
-echo e:\LiveLyricOverlay\fb2k_sdk\foobar2000\foo_sample\Release\foo_livelyric.dll
+echo e:\LiveLyricOverlay\fb2k_sdk_2025\foobar2000\foo_sample\x64\Release\foo_livelyric.dll
 echo.
 pause
--- fb2k_sdk/foobar2000/helpers/VolumeMap.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace VolumeMap {
-	double SliderToDB(double slider /*0..1 range*/);
-	double DBToSlider(double volumeDB);
-}
